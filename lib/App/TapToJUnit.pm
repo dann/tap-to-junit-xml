9fa15a73d9dd4d6349d0c6201d97b93ed76789d1
Dann <techmemo@gmail.com>
2011-12-24 23:43:14 +0900

    make tap-to-junit-xml script cpan module compatible

---
+++ lib/App/TapToJUnit.pm
@@ -0,0 +1,45 @@
+package App::TapToJUnit;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::TapToJUnit -
+
+=head1 SYNOPSIS
+
+  use App::TapToJUnit;
+
+=head1 DESCRIPTION
+
+App::TapToJUnit is
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+
+=head1 CONTRIBUTORS
+
+Many thanks to:
+
+
+=head1 AUTHOR
+
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
